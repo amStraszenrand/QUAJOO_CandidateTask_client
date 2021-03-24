d48100f8beb215b9c322fb118d87bba71f5788c3
klie <k.liebscher@studserv.uni-leipzig.de>
2021-03-24 12:07:25 +0100

    Initial commit -> Client works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { NewCityComponent } from './new-city/new-city.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewCityComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
